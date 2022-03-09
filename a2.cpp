3ebd07cd163a4d4a830ff0f96e49a99f1f7973df
YOSSEF SAIKO <98474220+YossefO@users.noreply.github.com>
2022-03-09 19:37:05 +0200

    Add files via upload

---
+++ a2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=7;i>1;i--){
+        for(int j=i;j<7;j++){
+            cout<<"#";
+        }
+        cout<<"\n";
+    }
+    
+    
+
+    return 0;
+}
