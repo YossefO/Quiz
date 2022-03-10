2427f70f609cfd960aa1ad236895acd30e815b3e
YOSSEF SAIKO <98474220+YossefO@users.noreply.github.com>
2022-03-10 20:27:54 +0200

    Add files via upload

---
+++ a4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    string age;
+    cout<<"Enter name  ";
+    cin>>name;
+    cout<<"Hello"<<" "<<name<<"\n";
+    cout<<"What's your age "<<" "<<name<<" ";
+    cin>>age;
+    cout<<"WOW"<<" "<<name;
+    return 0;
+}
