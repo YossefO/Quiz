a9a4ebc9b98da35bce41f4cd584d0cd49783cc35
YOSSEF SAIKO <98474220+YossefO@users.noreply.github.com>
2022-03-11 07:31:40 +0200

    Add files via upload

---
+++ a6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num1,num2;
+    cout<<"enter your frist number"<<" ";
+    cin>> num1;
+    cout<<"enter your second number"<<" ";
+    cin>> num2;
+
+    if (num1<num2){
+        cout<<num1<<"\n";
+    }
+
+    else
+        cout<<num2<<"\n";
+
+    return 0;
+}
