76674f621649289a23202fe6942f7b7a4fbfe627
YOSSEF SAIKO <98474220+YossefO@users.noreply.github.com>
2022-05-28 12:43:12 +0200

    Add files via upload

---
+++ homework1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double n1,n2;
+    cout<<"Enter";
+    cin>>n1>>n2; 
+    cout<<n1+n2<<endl;
+    
+    cout<<n1-n2<<endl;
+    cout<<n1*n2<<endl;
+    cout<<n1/n2<<endl;
+    cout << "Hello world!";
+
+    return 0;
+}
