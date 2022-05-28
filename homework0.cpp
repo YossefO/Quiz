d0d398a2eff9e5df9cde78ea46db55710e9498fd
YOSSEF SAIKO <98474220+YossefO@users.noreply.github.com>
2022-05-28 12:42:34 +0200

    Add files via upload

---
+++ homework0.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string na1;
+    string na2;
+    int id1;
+    int id2;
+    double ma1;
+    double ma2;
+
+
+
+    cout<<"What the student 1 name:"<<endl;
+    cin>>na1;
+    cout<<"His id:"<<endl;
+    cin>>id1;
+    cout<<"His math exam grade"<<endl;
+    cin>>ma1;
+
+    cout<<"What the student 2 name:"<<endl;
+    cin>>na2;
+    cout<<"His id:"<<endl;
+    cin>>id2;
+    cout<<"His math exam grade"<<endl;
+    cin>>ma2;
+
+    cout<<"Student grades in math"<<endl;
+    cout<<na1<<"(with id)"<<id1<<"got grade:"<<ma1<<endl;
+    cout<<na2<<"(with id)"<<id2<<"got grade:"<<ma2;
+
+
+
+
+
+    return 0;
+
+
+
+}
