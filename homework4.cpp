5ecfd72e132c166fdbd2ce726d27ec545fc241a1
YOSSEF Zack <98474220+YossefO@users.noreply.github.com>
2022-06-09 18:17:12 +0200

    Add files via upload

---
+++ homework4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double a,b,c,d,e;
+    cout<<"Enter 5 numbers"<<endl;
+    cin>>a>>b>>c>>d>>e;
+    cout<<(a+b+c)/(d+e);
+    return 0;
+}
